5b278b3bfbd800208d629667343f3cb551fc0074
Ahmet Şerefoğlu <ahmet.serefoglu46@gmail.com>
2015-07-15 02:27:32 +0300

    Revert "Php -Site"
    This reverts commit 44a8e4c58de76fabb3cc5cbb2511a49d8902398e.

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-	    include"adust.php";
-		if(isset($_SESSION["ad3"]))
-	    {   }
-	 	else
-		{
-		header('Location:index.php');
-		}
-?>
--- sqlbaglan.php
+++
@@ -1,5 +0,0 @@
-                        <?php
-						    $db= mysql_connect("localhost","root","") or die ("mysql baglantisi saglanamadi".mysql_error());
-                            $table= mysql_select_db("dersane",$db)or die ("mysql baglantisi saglanamadi".mysql_error());
-                            mysql_query("SET NAMES UTF8");
-						?>
